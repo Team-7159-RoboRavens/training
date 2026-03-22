26f3cdaf5e60917200b5d843355b9dc38202ea56
w9dolphin <w9dolphin@gmail.com>
2026-03-22 03:20:05 +0000

    Adjusted project configuration to make it work easier in github codespace

--- example_code/java-foundations/src/examples/Ex01_HelloWorld.java
+++ example_code/java-foundations/src/examples/Ex01_HelloWorld.java
@@ -4,7 +4,7 @@
  * The simplest Java program: printing to the console.
  * Every Java program needs a class with a main() method.
  * Run in github codespace by right-clicking the file and selecting "Run Java": try to switch to java 17 by sdk list java; sdk install java 17.0.18-amzn. After you restart codespace, you can right click on a file and run Java to run it
- * Run manually in command line: /workspaces/training/example_code/java-foundations; javac -d build/classes/java/main src/examples/Ex01_HelloWorld.java; java -cp build/classes/java/main Ex01_HelloWorld
+ * Run manually in command line: cd /workspaces/training/example_code/java-foundations/src/examples; javac Ex01_HelloWorld.java; java Ex01_HelloWorld
  * Run with: java -cp build/classes/java/main Ex01_HelloWorld
  */
 public class Ex01_HelloWorld {
